a91f525154f227e7c4acda9c70a55a4f01a35cb1
DMRobb77 <riffninja@live.com>
2023-06-27 12:38:48 -0500

    Set up and configure webpack Add webpack to the project and configure it so that it works with webpack. Add modules for font, css, and image support in webpack. Change file names and structure to better support webpack.

---
+++ src/book.js
@@ -0,0 +1,17 @@
+class Book {
+    constructor(
+        title = 'Empty',
+        author = 'Empty',
+        pages = '0',
+        date = 'Empty',
+        isRead = false
+    ) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.date = date;
+        this.isRead = isRead;
+    }
+}
+
+export default Book;
