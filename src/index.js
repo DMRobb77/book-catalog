ec5af0e2a40f6a3e9cfb9f99b109795ae1a0c324
DMRobb77 <riffninja@live.com>
2023-07-13 17:25:20 -0500

    Add JS logic and CSS classes for input validation Add basic input validation by disallowing empty inputs to be sent, then marking those inputs with a red border as well as displaying an error message when an empty submission is attempted. Rearrange some functions so that they're not called before they're declared as per ESLint warnings.

--- src/index.js
+++ src/index.js
@@ -1,34 +1,40 @@
 import Book from "./book";
-import initDOMSetup from './initial-layout';
-import 'normalize.css';
-import './style.css';
+import initDOMSetup from "./initial-layout";
+import "normalize.css";
+import "./style.css";
 
 initDOMSetup();
+
+const catalog = document.querySelector("#catalog");
+const modal = document.getElementById("modal");
+const modalOpener = document.getElementById("modal-opener");
+const cancelBtn = document.getElementById("cancel-btn");
+const addBtn = document.getElementById("add-btn");
 
 // Set up data structure
 
 class Library {
-    constructor() {
-        this.books = [];
-    }
+  constructor() {
+    this.books = [];
+  }
 
-    addBook(newBook) {
-        if (!this.isInLibrary(newBook)) {
-            this.books.push(newBook);
-        }
+  addBook(newBook) {
+    if (!this.isInLibrary(newBook)) {
+      this.books.push(newBook);
     }
+  }
 
-    getBook(title) {
-        return this.books.find((book) => book.title === title);
-    }
+  getBook(title) {
+    return this.books.find((book) => book.title === title);
+  }
 
-    removeBook(title) {
-        this.books = this.books.filter((book) => book.title !== title);
-    }
+  removeBook(title) {
+    this.books = this.books.filter((book) => book.title !== title);
+  }
 
-    isInLibrary(newBook) {
-        return this.books.some((book) => book.title === newBook.title);
-    }
+  isInLibrary(newBook) {
+    return this.books.some((book) => book.title === newBook.title);
+  }
 }
 
 const library = new Library();
@@ -36,152 +42,165 @@ const library = new Library();
 // UI manipulation
 
 const removeBookFromLibrary = (e) => {
-    console.log(e.target.closest('.book-card').getAttribute('index'));
-    const removedIndex = e.target.closest('.book-card').getAttribute('index');
-    const removedBook = library.books.splice(removedIndex, 1);
-    e.target.closest('.book-card').remove();
-    console.log(library.books);
+  e.target.closest(".book-card").remove();
+  const cards = document.getElementsByClassName("book-card");
 
-    const cards = document.getElementsByClassName('book-card');
-
-    for (let i = 0; i < cards.length; i++) {
-        cards[i].setAttribute('index', i);
-    }
-
-}
+  for (let i = 0; i < cards.length; i += 1) {
+    cards[i].setAttribute("index", i);
+  }
+};
 
 const checkboxAlert = (e) => {
+  const checkedIndex = e.target.closest(".book-card").getAttribute("index");
+  const isCheckedRead = library.books[checkedIndex].isRead;
 
-    const checkedIndex = e.target.closest('.book-card').getAttribute('index');
-    const isCheckedRead = library.books[checkedIndex].isRead;
-
-    if (isCheckedRead == true){
-        library.books[checkedIndex].isRead = false;
-    }
-    else {
-        library.books[checkedIndex].isRead = true;
-    }
-    
-    console.log(library.books[checkedIndex].isRead);
-
-}
+  if (isCheckedRead === true) {
+    library.books[checkedIndex].isRead = false;
+  } else {
+    library.books[checkedIndex].isRead = true;
+  }
+};
 
 const addBookToDisplay = (book) => {
+  const bookCard = document.createElement("div");
+  bookCard.className = "book-card";
+  catalog.appendChild(bookCard);
 
-    const bookCard = document.createElement('div');
-    bookCard.className = 'book-card';
-    catalog.appendChild(bookCard);
+  const cardHeader = document.createElement("div");
+  cardHeader.className = "card-header";
+  bookCard.appendChild(cardHeader);
 
-    const cardHeader = document.createElement('div');
-    cardHeader.className = 'card-header';
-    bookCard.appendChild(cardHeader);
+  const title = document.createElement("h4");
+  title.className = "book-title";
+  title.innerHTML = book.title;
 
-    const title = document.createElement('h4');
-    title.className = 'book-title';
-    title.innerHTML = book.title;
+  const author = document.createElement("h4");
+  author.className = "author-name";
+  author.innerHTML = `By: ${book.author}`;
 
-    const author = document.createElement('h4');
-    author.className = 'author-name';
-    author.innerHTML = `By: ${book.author}`;
+  const pages = document.createElement("h4");
+  pages.className = "page-count";
+  pages.innerHTML = `Length: ${book.pages} pages`;
 
-    const pages = document.createElement('h4');
-    pages.className = 'page-count';
-    pages.innerHTML = `Length: ${book.pages} pages`;
+  const date = document.createElement("h4");
+  date.className = "published";
+  date.innerHTML = `Published: ${book.date}`;
 
-    const date = document.createElement('h4');
-    date.className = 'published';
-    date.innerHTML = `Published: ${book.date}`;
+  const isReadDiv = document.createElement("div");
+  isReadDiv.className = "has-read";
 
-    const isReadDiv = document.createElement('div');
-    isReadDiv.className = 'has-read';
+  const isReadText = document.createElement("h4");
+  isReadText.innerHTML = "Finished reading?";
+  isReadDiv.appendChild(isReadText);
 
-    const isReadText = document.createElement('h4');
-    isReadText.innerHTML = 'Finished reading?';
-    isReadDiv.appendChild(isReadText);
+  const isReadBox = document.createElement("label");
+  const isReadSwitch = document.createElement("input");
+  const isReadSlider = document.createElement("span");
+  isReadBox.className = "switch";
+  isReadSlider.className = "slider";
+  isReadSwitch.setAttribute("type", "checkbox");
+  isReadSwitch.addEventListener("click", checkboxAlert);
+  isReadBox.appendChild(isReadSwitch);
+  isReadBox.appendChild(isReadSlider);
+  isReadDiv.appendChild(isReadBox);
 
-    const isReadBox = document.createElement('label');
-    const isReadSwitch = document.createElement('input');
-    const isReadSlider = document.createElement('span');
-    isReadBox.className = 'switch';
-    isReadSlider.className = 'slider';
-    isReadSwitch.setAttribute('type', 'checkbox');
-    isReadSwitch.addEventListener('click', checkboxAlert);
-    isReadBox.appendChild(isReadSwitch);
-    isReadBox.appendChild(isReadSlider);
-    isReadDiv.appendChild(isReadBox);
+  const deleteButton = document.createElement("div");
+  deleteButton.className = "delete";
+  const deleteText = document.createElement("span");
+  deleteText.innerHTML = "Remove";
+  deleteButton.appendChild(deleteText);
+  deleteButton.addEventListener("click", removeBookFromLibrary);
 
-    const deleteButton = document.createElement('div');
-    deleteButton.className = 'delete';
-    const deleteText = document.createElement('span');
-    deleteText.innerHTML = "Remove";
-    deleteButton.appendChild(deleteText);
-    deleteButton.addEventListener('click', removeBookFromLibrary);
+  bookCard.setAttribute("index", library.books.indexOf(book));
 
+  cardHeader.appendChild(title);
+  bookCard.appendChild(author);
+  bookCard.appendChild(pages);
+  bookCard.appendChild(date);
+  bookCard.appendChild(isReadDiv);
+  bookCard.appendChild(deleteButton);
 
-
-    bookCard.setAttribute('index', library.books.indexOf(book));    
-    
-    cardHeader.appendChild(title);
-    bookCard.appendChild(author);
-    bookCard.appendChild(pages);
-    bookCard.appendChild(date);
-    bookCard.appendChild(isReadDiv);
-    bookCard.appendChild(deleteButton);
-
-    closeModal();
-    clearBookInputFields();
-}
+  closeModal();
+  clearBookInputFields();
+};
 
 const clearBookInputFields = () => {
-    document.getElementById('input-title').value = "";
-    document.getElementById('input-author').value = "";
-    document.getElementById('input-pages').value = "";
-    document.getElementById('input-date').value = "";
-    document.getElementById('input-finished').value = false;
-
-}
+  document.getElementById("input-title").value = "";
+  document.getElementById("input-author").value = "";
+  document.getElementById("input-pages").value = "";
+  document.getElementById("input-date").value = "";
+  document.getElementById("input-finished").value = false;
+};
 
 // Adding book data
 
 const getBookFromInput = () => {
-    const title = document.getElementById('input-title').value;
-    const author = document.getElementById('input-author').value;
-    const pages = document.getElementById('input-pages').value;
-    const date = document.getElementById('input-date').value;
-    const hasRead = document.getElementById('input-finished').value;
-    return new Book(title, author, pages, date, hasRead);
+  const title = document.getElementById("input-title").value;
+  const author = document.getElementById("input-author").value;
+  const pages = document.getElementById("input-pages").value;
+  const date = document.getElementById("input-date").value;
+  const hasRead = document.getElementById("input-finished").value;
 
+  return new Book(title, author, pages, date, hasRead);
+};
+
+const errorMessage = document.getElementById("error-msg");
+const inputs = document.getElementsByTagName("input");
+
+const handleInputFocus = () => {
+  for (let i = 0; i < inputs.length; i += 1) {
+    inputs[i].classList.remove("needs-completed");
+  }
+};
+
+for (let i = 0; i < inputs.length; i += 1) {
+  inputs[i].addEventListener("focus", handleInputFocus);
 }
 
 const addBookToLibrary = (e) => {
-    const newBook = getBookFromInput();
+  const newBook = getBookFromInput();
+  let isValid = true;
+
+  for (let i = 0; i < inputs.length; i += 1) {
+    if (inputs[i].hasAttribute("required") && inputs[i].value === "") {
+      isValid = false;
+      inputs[i].classList.toggle("needs-completed");
+    }
+  }
+  if (!isValid) {
+    errorMessage.style.display = "block";
+  } else {
+    errorMessage.style.display = "none";
     library.addBook(newBook);
     addBookToDisplay(newBook);
-
-}
-
-const catalog = document.querySelector('#catalog');
-
-const modal = document.getElementById('modal');
-const modalOpener = document.getElementById('modal-opener');
-const cancelBtn = document.getElementById('cancel-btn');
-const addBtn = document.getElementById('add-btn');
+  }
+};
 
 const openModal = () => {
-    modal.style.display = "block";
-}
+  modal.style.display = "block";
+};
 
 const closeModal = () => {
-    modal.style.display = "none";
-}
+  errorMessage.style.display = "none";
+  modal.style.display = "none";
+  for (let i = 0; i < inputs.length; i += 1) {
+    inputs[i].value = "";
+  }
+  handleInputFocus();
+};
 
 modalOpener.addEventListener("click", openModal);
 addBtn.addEventListener("click", addBookToLibrary);
 cancelBtn.addEventListener("click", closeModal);
 
-
 // Automatically adding test book
 
-const hobbit = new Book('The Hobbit', 'JRR Tolkien', 433, 'June 14 2004', 'not read yet');
+const hobbit = new Book(
+  "The Hobbit",
+  "JRR Tolkien",
+  433,
+  "June 14 2004",
+  "not read yet"
+);
 library.addBook(hobbit);
 addBookToDisplay(hobbit);
--- src/initial-layout.js
+++ src/initial-layout.js
@@ -1,135 +1,149 @@
-
 const initDOMSetup = () => {
-    // Create the title-holder div
-    const titleHolderDiv = document.createElement("div");
-    titleHolderDiv.id = "title-holder";
+  // Create the title-holder div
+  const titleHolderDiv = document.createElement("div");
+  titleHolderDiv.id = "title-holder";
 
-    // Create the title heading element
-    const titleHeading = document.createElement("h2");
-    titleHeading.id = "title";
-    titleHeading.textContent = "Personal Library";
+  // Create the title heading element
+  const titleHeading = document.createElement("h2");
+  titleHeading.id = "title";
+  titleHeading.textContent = "Personal Library";
 
-    // Append the title heading to the title-holder div
-    titleHolderDiv.appendChild(titleHeading);
+  // Append the title heading to the title-holder div
+  titleHolderDiv.appendChild(titleHeading);
 
-    // Append the title-holder div to the document body
-    document.body.appendChild(titleHolderDiv);
+  // Append the title-holder div to the document body
+  document.body.appendChild(titleHolderDiv);
 
-    // Create the modal element
-    const modalDiv = document.createElement("div");
-    modalDiv.id = "modal";
+  // Create the modal element
+  const modalDiv = document.createElement("div");
+  modalDiv.id = "modal";
 
-    // Create the book-entry element
-    const bookEntryDiv = document.createElement("div");
-    bookEntryDiv.id = "book-entry";
+  // Create the book-entry element
+  const bookEntryDiv = document.createElement("div");
+  bookEntryDiv.id = "book-entry";
 
-    // Create the input-fields element
-    const inputFieldsDiv = document.createElement("div");
-    inputFieldsDiv.id = "input-fields";
+  // Create the input-fields element
+  const inputFieldsForm = document.createElement("form");
+  inputFieldsForm.id = "input-fields";
 
-    // Create the title input field
-    const inputTitle = document.createElement("input");
-    inputTitle.type = "text";
-    inputTitle.id = "input-title";
-    inputTitle.name = "input-title";
-    inputTitle.placeholder = "Title";
-    inputFieldsDiv.appendChild(inputTitle);
+  // Create the title input field
+  const inputTitle = document.createElement("input");
+  inputTitle.type = "text";
+  inputTitle.id = "input-title";
+  inputTitle.name = "input-title";
+  inputTitle.placeholder = "Title";
+  inputFieldsForm.appendChild(inputTitle);
 
-    // Create the author input field
-    const inputAuthor = document.createElement("input");
-    inputAuthor.type = "text";
-    inputAuthor.id = "input-author";
-    inputAuthor.name = "input-author";
-    inputAuthor.placeholder = "Author";
-    inputFieldsDiv.appendChild(inputAuthor);
+  // Create the author input field
+  const inputAuthor = document.createElement("input");
+  inputAuthor.type = "text";
+  inputAuthor.id = "input-author";
+  inputAuthor.name = "input-author";
+  inputAuthor.placeholder = "Author";
+  inputFieldsForm.appendChild(inputAuthor);
 
-    // Create the pages input field
-    const inputPages = document.createElement("input");
-    inputPages.type = "number";
-    inputPages.id = "input-pages";
-    inputPages.name = "input-pages";
-    inputPages.placeholder = "Page count";
-    inputPages.setAttribute("oninput", "this.value = this.value.replace(/[^0-9]/g, '').replace(/(\\..*)\\./g,'$1');");
-    inputFieldsDiv.appendChild(inputPages);
+  // Create the pages input field
+  const inputPages = document.createElement("input");
+  inputPages.type = "number";
+  inputPages.id = "input-pages";
+  inputPages.name = "input-pages";
+  inputPages.placeholder = "Page count";
+  inputPages.setAttribute(
+    "oninput",
+    "this.value = this.value.replace(/[^0-9]/g, '').replace(/(\\..*)\\./g,'$1');"
+  );
+  inputFieldsForm.appendChild(inputPages);
 
-    // Create the date published label
-    const datePublishedLabel = document.createElement("label");
-    datePublishedLabel.htmlFor = "input-date";
-    datePublishedLabel.textContent = "Date published:";
-    inputFieldsDiv.appendChild(datePublishedLabel);
+  // Create the date published label
+  const datePublishedLabel = document.createElement("label");
+  datePublishedLabel.htmlFor = "input-date";
+  datePublishedLabel.textContent = "Date published:";
+  inputFieldsForm.appendChild(datePublishedLabel);
 
-    // Create the date published input field
-    const inputDate = document.createElement("input");
-    inputDate.type = "date";
-    inputDate.id = "input-date";
-    inputDate.name = "input-date";
-    inputDate.placeholder = "Date published";
-    inputFieldsDiv.appendChild(inputDate);
+  // Create the date published input field
+  const inputDate = document.createElement("input");
+  inputDate.type = "date";
+  inputDate.id = "input-date";
+  inputDate.name = "input-date";
+  inputDate.placeholder = "Date published";
+  inputFieldsForm.appendChild(inputDate);
 
-    // Create the "Finished reading?" div
-    const finishedReadingDiv = document.createElement("div");
-    finishedReadingDiv.id = "finished-reading";
-    inputFieldsDiv.appendChild(finishedReadingDiv);
+  // Create the "Finished reading?" div
+  const finishedReadingDiv = document.createElement("div");
+  finishedReadingDiv.id = "finished-reading";
+  inputFieldsForm.appendChild(finishedReadingDiv);
 
-    // Create the "Finished reading?" label
-    const finishedReadingLabel = document.createElement("label");
-    finishedReadingLabel.htmlFor = "input-finished";
-    finishedReadingLabel.textContent = "Finished reading?";
-    finishedReadingDiv.appendChild(finishedReadingLabel);
+  // Create the "Finished reading?" label
+  const finishedReadingLabel = document.createElement("label");
+  finishedReadingLabel.htmlFor = "input-finished";
+  finishedReadingLabel.textContent = "Finished reading?";
+  finishedReadingDiv.appendChild(finishedReadingLabel);
 
-    // Create the "Finished reading?" checkbox
-    const inputFinished = document.createElement("input");
-    inputFinished.type = "checkbox";
-    inputFinished.id = "input-finished";
-    inputFinished.name = "input-finished";
-    finishedReadingDiv.appendChild(inputFinished);
+  // Create the "Finished reading?" checkbox
+  const inputFinished = document.createElement("input");
+  inputFinished.type = "checkbox";
+  inputFinished.id = "input-finished";
+  inputFinished.name = "input-finished";
+  finishedReadingDiv.appendChild(inputFinished);
 
-    // Append the input-fields element to the book-entry element
-    bookEntryDiv.appendChild(inputFieldsDiv);
+  // Add simple validation to the forms
+  inputTitle.required = true;
+  inputAuthor.required = true;
+  inputPages.required = true;
+  inputDate.required = true;
 
-    // Create the modal-btn-holder element
-    const modalBtnHolderDiv = document.createElement("div");
-    modalBtnHolderDiv.id = "modal-btn-holder";
+  // Append the input-fields element to the book-entry element
+  bookEntryDiv.appendChild(inputFieldsForm);
 
-    // Create the "Add Book" button
-    const addBtn = document.createElement("button");
-    addBtn.name = "add-btn";
-    addBtn.type = "button";
-    addBtn.id = "add-btn";
-    addBtn.className = "button";
-    addBtn.textContent = "Add Book";
-    modalBtnHolderDiv.appendChild(addBtn);
+  // Create the modal-btn-holder element
+  const modalBtnHolderDiv = document.createElement("div");
+  modalBtnHolderDiv.id = "modal-btn-holder";
 
-    // Create the "Cancel" button
-    const cancelBtn = document.createElement("button");
-    cancelBtn.name = "cancel-btn";
-    cancelBtn.id = "cancel-btn";
-    cancelBtn.type = "button";
-    cancelBtn.className = "button";
-    cancelBtn.textContent = "Cancel";
-    modalBtnHolderDiv.appendChild(cancelBtn);
+  // Create the "Add Book" button
+  const addBtn = document.createElement("button");
+  addBtn.name = "add-btn";
+  addBtn.type = "button";
+  addBtn.id = "add-btn";
+  addBtn.className = "button";
+  addBtn.textContent = "Add Book";
+  modalBtnHolderDiv.appendChild(addBtn);
 
-    // Append the modal-btn-holder element to the book-entry element
-    bookEntryDiv.appendChild(modalBtnHolderDiv);
+  // Create the "Cancel" button
+  const cancelBtn = document.createElement("button");
+  cancelBtn.name = "cancel-btn";
+  cancelBtn.id = "cancel-btn";
+  cancelBtn.type = "button";
+  cancelBtn.className = "button";
+  cancelBtn.textContent = "Cancel";
+  modalBtnHolderDiv.appendChild(cancelBtn);
 
-    // Append the book-entry element to the modal element
-    modalDiv.appendChild(bookEntryDiv);
+  // Append the modal-btn-holder element to the book-entry element
+  inputFieldsForm.appendChild(modalBtnHolderDiv);
 
-    // Get the catalog element
-    const catalogDiv = document.createElement("div");
-    catalogDiv.id = "catalog";
-    document.body.appendChild(catalogDiv);
+  // Create a message that will display if the book can't be submitted
+  const errorMessage = document.createElement("div");
+  errorMessage.textContent = "Please fill in all required fields.";
+  errorMessage.id = "error-msg";
+  inputFieldsForm.appendChild(errorMessage);
 
-    // Create the "Add Book" button in the catalog
-    const modalOpenerBtn = document.createElement("button");
-    modalOpenerBtn.name = "modal-opener";
-    modalOpenerBtn.type = "button";
-    modalOpenerBtn.id = "modal-opener";
-    modalOpenerBtn.textContent = "+";
-    catalogDiv.appendChild(modalOpenerBtn);
+  // Append the book-entry element to the modal element
+  modalDiv.appendChild(bookEntryDiv);
 
-    // Append the modal element to the document body
-    document.body.appendChild(modalDiv);
-}
+  // Get the catalog element
+  const catalogDiv = document.createElement("div");
+  catalogDiv.id = "catalog";
+  document.body.appendChild(catalogDiv);
+
+  // Create the "Add Book" button in the catalog
+  const modalOpenerBtn = document.createElement("button");
+  modalOpenerBtn.name = "modal-opener";
+  modalOpenerBtn.type = "button";
+  modalOpenerBtn.id = "modal-opener";
+  modalOpenerBtn.textContent = "+";
+  catalogDiv.appendChild(modalOpenerBtn);
+
+  // Append the modal element to the document body
+  document.body.appendChild(modalDiv);
+};
 
 export default initDOMSetup;
